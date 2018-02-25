532cb9e8fd6888e95ce545b28f04f1ac3378d426
silentknight <1491628+silentknight@users.noreply.github.com>
2018-02-25 10:14:25 +0000

    Changes on 25 Feb 2018

--- run_all.py
+++ run_all.py
@@ -13,6 +13,7 @@ import gzip
 import pickle
 import copy
 import argparse
+import time
 
 # Setup matplotlib renderer to avoid using the screen (must be done early)
 import matplotlib
@@ -25,70 +26,23 @@ import numpy as np
 import data
 import mutual_information as mi
 
-parser = argparse.ArgumentParser(description='Long Distance Dependency measurements')
-parser.add_argument('--data', type=str, default='dataset/dl4mt/', help='location of the data corpus')
-parser.add_argument('--model', type=str, default='LSTM', help='type of recurrent net (LSTM, QRNN, GRU)')
-args = parser.parse_args()
-
-###############################################################################
-# Load data
-###############################################################################
-
-corpus = data.Corpus(args.data)
-
-###############################################################################
-# Calculate Mutual Information
-###############################################################################
-
-ldd = mi.MutualInformation(corpus)
-
-
-def pmi_Penn_Tree_Bank_data():
-	data_Dir = "dataset/penn-tree-bank/treebank/raw"
-
-	sequential_Data = ""
-
-	for root, dirs, files in os.walk(data_Dir):
-		for fname in sorted(files):
-			file_Name = os.path.join(root, fname)
-
-			data_File = open(file_Name, "r")
-			lines = data_File.readlines()
-			data_File.close()
-
-			valid_File = False
-			for line in lines:
-				if line.strip() == ".START":
-					valid_File = True
-				else:
-					if valid_File == True:
-						sequential_Data += line.strip()
-
-	print(sequential_Data)
-	print(len(sequential_Data))
-
-def pmi_SP_Data():
-	data_Dir_Original = "dataset/foma/Original_Data"
-	data_Dir_Generated = "dataste/foma/Generated_Data"
-
-	for root, dirs, files in os.walk(data_Dir_Original):
-		for fname in sorted(files):
-			file_Name = os.path.join(root, fname)
-			# print(file_Name)
-
-
-def pmi_wikiText2_Data():
-	pass
-	
-
-def pmi_wikiText103_Data():
-	pass
-
 def main():
-	pmi_Penn_Tree_Bank_data()
-	pmi_SP_Data()
-	pmi_wikiText2_Data()
-	pmi_wikiText103_Data()
+	parser = argparse.ArgumentParser(description='Long Distance Dependency measurements')
+	parser.add_argument('--data', type=str, default='dataset/dl4mt/', help='location of the data corpus')
+	parser.add_argument('--model', type=str, default='LSTM', help='type of recurrent net (LSTM, QRNN, GRU)')
+	args = parser.parse_args()
 
-# if __name__ == '__main__':
-# 	main()
+	###############################################################################
+	# Load data
+	###############################################################################
+
+	corpus = data.Corpus(args.data)
+
+	###############################################################################
+	# Calculate Mutual Information
+	###############################################################################
+
+	ldd = mi.MutualInformation(corpus)
+
+if __name__ == '__main__':
+	main()
