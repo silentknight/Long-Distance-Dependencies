648ae77fc26cf769e047fca2743cb4b5a62b09d2
silentknight <1491628+silentknight@users.noreply.github.com>
2018-02-25 22:07:41 +0000

    Changed from iteration heavy to memory heavy

--- mutual_information.py
+++ mutual_information.py
@@ -3,6 +3,7 @@
 # System libs
 from __future__ import print_function
 import numpy as np
+import time
 
 class MutualInformation(object):
 	def __init__(self, corpusData):
@@ -14,4 +15,21 @@ class MutualInformation(object):
 			px = np.append(px, corpus.dictionary.counter[counts])
 		px = px/corpus.sequentialData.total
 
-		pmi = 
+		end = True
+		d = 1
+		pair_counts = 0
+
+		print(time.time())
+		while d<corpus.sequentialData.total and end==True:
+			for i in range(len(corpus.dictionary.counter)):
+				for j in range(len(corpus.dictionary.counter)):
+					for index in range(corpus.sequentialData.total):
+						print(corpus.sequentialData.wordArray[index], i, corpus.sequentialData.wordArray[index+d], j)
+						if corpus.sequentialData.wordArray[index] == i and corpus.sequentialData.wordArray[index+d] == j:
+							pair_counts += 1
+							print("Present: ", corpus.dictionary.idx2word[i], corpus.dictionary.idx2word[j], pair_counts)
+			d += 1
+			pair_counts = 0
+			print(d, time.time())
+
+		print("Done", time.time())
