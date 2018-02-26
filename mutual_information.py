31fc6e58ce91fbc7d90142ff0003547665696f2d
silentknight <1491628+silentknight@users.noreply.github.com>
2018-02-26 11:42:39 +0000

    Working code for Mutual Information

--- mutual_information.py
+++ mutual_information.py
@@ -10,26 +10,34 @@ class MutualInformation(object):
 		self.mutualInformation = self.calculate_MI(corpusData)
 
 	def calculate_MI(self, corpus):
-		px = np.empty([0,1])
+		pair_counts = np.zeros([len(corpus.dictionary.counter),len(corpus.dictionary.counter)])
+		pmi = np.zeros([len(corpus.dictionary.counter),len(corpus.dictionary.counter)])
+		px = np.zeros([0,1])
+		mi = np.zeros([0,1])
+
 		for counts in corpus.dictionary.counter:
 			px = np.append(px, corpus.dictionary.counter[counts])
-		px = px/corpus.sequentialData.total
+		px = px.reshape(1,len(px))/corpus.sequentialData.total
+		pxpy = px*np.transpose(px)
 
-		end = True
+		end = False
 		d = 1
-		pair_counts = 0
+		while d<corpus.sequentialData.total and end==False:
+			for index in range(corpus.sequentialData.total):
+				if index+d>=corpus.sequentialData.total:
+					break
+				pair_counts[corpus.sequentialData.wordArray[index],corpus.sequentialData.wordArray[index+d]]+=1
 
-		print(time.time())
-		while d<corpus.sequentialData.total and end==True:
-			for i in range(len(corpus.dictionary.counter)):
-				for j in range(len(corpus.dictionary.counter)):
-					for index in range(corpus.sequentialData.total):
-						print(corpus.sequentialData.wordArray[index], i, corpus.sequentialData.wordArray[index+d], j)
-						if corpus.sequentialData.wordArray[index] == i and corpus.sequentialData.wordArray[index+d] == j:
-							pair_counts += 1
-							print("Present: ", corpus.dictionary.idx2word[i], corpus.dictionary.idx2word[j], pair_counts)
+			# PMI is Pointwose Mutual Information calcutaed between joint probability of two Random Variables and marginal probability of the same Random Variables
+			joint_prob = pair_counts/corpus.sequentialData.total
+			temp = joint_prob/pxpy
+			# Removing 0's to avoid log(0)
+			temp[temp==0] = 1
+			pmi = np.log2(temp)
+
+			# Mutual Information is Expected Value over all the Pointwise Mutual Information
+			mi = (joint_prob*pmi).sum()
+			print(d, mi)
+
 			d += 1
-			pair_counts = 0
-			print(d, time.time())
-
-		print("Done", time.time())
+			pair_counts = np.zeros([len(corpus.dictionary.counter),len(corpus.dictionary.counter)])
