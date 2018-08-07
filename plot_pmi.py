efbafdb60ee0cce2d383802219d04ff160c74e89
silentknight <1491628+silentknight@users.noreply.github.com>
2018-08-07 22:18:16 +0100

    PMI plot changes

--- plot_pmi.py
+++ plot_pmi.py
@@ -5,26 +5,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import sys
+import os
 
 parser = argparse.ArgumentParser(description='Long Distance Dependency measurements plot')
-parser.add_argument('--path', type=str, default='ldd_data.dat', help='path of the data file')
+parser.add_argument('--path', type=str, default='pmi_data/log_data', help='Path of the data file')
 parser.add_argument('--start', type=int, default=1, help='Value of D (Dependency distance) from start for plotting. Default is 1')
 parser.add_argument('--end', type=str, default='end', help='Value of D (Dependency distance) till end for plotting. Default is \'end\' (all the way will the end).')
 parser.add_argument('--logscale', type=int, default=1, help='Plot on Log Scale or normal scale. 1: Log Scale; 0: Normal Scale')
 parser.add_argument('--normalize', type=str, default='n', help='normalize values')
 args = parser.parse_args()
-	
-try:
-	f = open(args.path, "r")
-	lines = f.readlines()
-	f.close()
-except FileNotFoundError:
-	print("No such file exists. Default file is ldd_data.dat")
-	sys.exit()
 
 start = 0
 end = 0
-index = 0
 
 if args.start > 0:
 	start = args.start
@@ -40,61 +32,69 @@ else:
 	except ValueError:
 		print("Not a well formed integer")
 		sys.exit()
+	
+try:
+	d_num = []
+	ext = ""
+	files = sorted(os.listdir(args.path))
+	for file in files:
+		d_num.append(int(file.split('.')[0]))
+		ext = file.split('.')[1]
 
-mi = np.zeros([0,1])
-Hx = np.zeros([0,1])
-Hy = np.zeros([0,1])
-Hxy = np.zeros([0,1])
-temp = lines[0].split()
-if temp[0] == "data:":
-	for line in lines:
-		temp = line.strip().split(":")
-		if temp[0] == "d":
-			temp1 = temp[2].split(",")				
-			mi = np.append(mi,np.zeros(1))
-			mi[int(temp[1])-1] = float(temp1[0])
-			Hx = np.append(Hx,np.zeros(1))
-			Hx[int(temp[1])-1] = float(temp1[1])
-			Hy = np.append(Hy,np.zeros(1))
-			Hy[int(temp[1])-1] = float(temp1[2])
-			Hxy = np.append(Hxy,np.zeros(1))
-			Hxy[int(temp[1])-1] = float(temp1[3])
-			d = int(temp[1])+1
-		index+=1
-else:
-	print("Not a valid file")
+	d_num = sorted(d_num)
 
-if args.normalize == "y" or args.normalize == "Y":
-	x = mi/np.amax(mi)
-else:
-	x = mi
+	files = []
+	for file in d_num:
+		if file >= start and file <= end:
+			files.append(str(file)+'.'+ext)
 
-plt.subplot(221)
-if args.logscale == 1:
-	plt.loglog(np.arange(len(x)),x,basex=10)
-elif args.logscale == 0:
-	plt.plot(x)
-plt.grid(True)
+except  Exception as e:
+	print(e)
+	print(args.path+" does not exist")
 
-plt.subplot(222)
-if args.logscale == 1:
-	plt.semilogx(np.arange(len(Hxy)),Hxy)
-elif args.logscale == 0:
-	plt.plot(Hxy)
-plt.grid(True)
+d = start
 
-plt.subplot(223)
-if args.logscale == 1:
-	plt.semilogx(np.arange(len(Hx)),Hx)
-elif args.logscale == 0:
-	plt.plot(Hx)
-plt.grid(True)
+for file in files:
+	npzfile = np.load(file)
+	Xi = npzfile['arr_0']
+	Yi = npzfile['arr_1']
+	Ni_X = npzfile['arr_2']
+	Ni_Y = npzfile['arr_3']
+	Ni_XY = npzfile['arr_4']
+	pmi = npzfile['arr_5']
 
-plt.subplot(224)
-if args.logscale == 1:
-	plt.semilogx(np.arange(len(Hy)),Hy)
-elif args.logscale == 0:
-	plt.plot(Hy)
-plt.grid(True)
+	
+# if args.normalize == "y" or args.normalize == "Y":
+# 	x = mi/np.amax(mi)
+# else:
+# 	x = mi
 
-plt.show()
+# plt.subplot(221)
+# if args.logscale == 1:
+# 	plt.loglog(np.arange(len(x)),x,basex=10)
+# elif args.logscale == 0:
+# 	plt.plot(x)
+# plt.grid(True)
+
+# plt.subplot(222)
+# if args.logscale == 1:
+# 	plt.semilogx(np.arange(len(Hxy)),Hxy)
+# elif args.logscale == 0:
+# 	plt.plot(Hxy)
+# plt.grid(True)
+
+# plt.subplot(223)
+# if args.logscale == 1:
+# 	plt.semilogx(np.arange(len(Hx)),Hx)
+# elif args.logscale == 0:
+# 	plt.plot(Hx)
+# plt.grid(True)
+
+# plt.subplot(224)
+# if args.logscale == 1:
+# 	plt.semilogx(np.arange(len(Hy)),Hy)
+# elif args.logscale == 0:
+# 	plt.plot(Hy)
+# plt.grid(True)
+
+# plt.show()
