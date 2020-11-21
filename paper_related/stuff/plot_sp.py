5f43b5fa94f2b05af7640eb88716c018ef0dc8a2
Abhijit Mahalunkar <1491628+silentknight@users.noreply.github.com>
2020-11-21 20:24:11 +0000

    Update plot_sp.py

--- paper_related/stuff/plot_sp.py
+++ paper_related/stuff/plot_sp.py
@@ -3,14 +3,13 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.interpolate import spline
 
-# filenames = ["sp2_20_v26_char","sp2_20_char","sp2_100_char","sp2_200_char","sp2_500_char","sp4_20_char","sp4_100_char","sp16_100_char"]
-filenames = ["sp2_20_char","sp2_20_v26_char"]
+filenames = ["sp2_20_char","sp2_100_char","sp2_200_char","sp2_500_char","sp4_100_char","sp16_100_char","sp2_20_v26_char","sp2_20_char_f_mi","sp2_20_char_size_mi"]
+
 all_mi = []
 
 for filename in filenames:
-    f = open("../datapoints/"+filename+".log", "r")
+    f = open(filename+".log", "r")
     lines = f.readlines()
     f.close()
 
@@ -41,23 +40,31 @@ for filename in filenames:
     else:
         print("Not a valid file")
 
-    all_mi.append(np.abs(mi[0:1000]).tolist())
+    all_mi.append(np.abs(mi).tolist())
 
 # red dashes, blue squares and green triangles
 with plt.style.context(('seaborn')):
-    p1, p2 = plt.loglog(np.arange(len(all_mi[0])), all_mi[0], np.arange(len(all_mi[1])), all_mi[1])
-    plt.tick_params(labelsize='large', width=3)
+    plt.loglog(np.arange(1,len(all_mi[0])+1), all_mi[0], label="SP2 20")
+    plt.loglog(np.arange(1,len(all_mi[1])+1), all_mi[1], label="SP2 100")
+    plt.loglog(np.arange(1,len(all_mi[2])+1), all_mi[2], label="SP2 200")
+    plt.loglog(np.arange(1,len(all_mi[3])+1), all_mi[3], label="SP2 500")
+    plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], label="SP4 100")
+    plt.loglog(np.arange(1,len(all_mi[5])+1), all_mi[5], label="SP16 100")
+    plt.loglog(np.arange(1,len(all_mi[6])+1), all_mi[6], label="SP2 20 V26")
+    plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], label="SP2 20 FS={ab,bc,cd,dc}")
+    plt.loglog(np.arange(1,len(all_mi[8])+1), all_mi[8], label="SP2 20 Small")
+
+    plt.tick_params(labelsize='large', width=5)
     plt.grid(True)
     plt.grid(which='major', linestyle='-.', linewidth='0.5', color='grey')
     plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
-    plt.ylim(-0.005, 0.06)
-    plt.xlim(0, 1000)
-
-
     ax = plt.axes()
-    ax.set_xlabel('Distance between two symbols, D(X,Y)', fontsize=15)
-    ax.set_ylabel('Mutual Information, I(X,Y)', fontsize=15)
-    lgd = ax.legend((p1, p2), ("SP2 20 v4","SP2 20 v26"), loc='lower left', shadow=True, fancybox=True)
-
-    # plt.savefig('spk_k', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    ax.set_xlim(1, len(all_mi[0]))
+    ax.set_ylim(-0.005, 0.06)
+    print(ax.get_xlim())
+    print(ax.get_ylim())
+    ax.set_xlabel('Distance between symbols, d', fontsize=15)
+    ax.set_ylabel('Mutual Information, I(d)', fontsize=15)
+    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, ncol=2, numpoints=1, prop={'size': 12})
+    plt.savefig('spk_', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
