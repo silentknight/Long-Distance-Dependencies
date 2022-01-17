1a6580aa45de6caf249a1ace56640fecac5f3194
silentknight <1491628+silentknight@users.noreply.github.com>
2022-01-17 13:47:37 +0000

    Final images

--- paper_related/spk/plot_sp.py
+++ paper_related/spk/plot_sp.py
@@ -53,28 +53,23 @@ with plt.style.context(('seaborn')):
     # plt.loglog(np.arange(1,len(all_mi[5])+1), all_mi[5], label="SP4, L=100, V=4, FS={abba,bccb}")
     # plt.loglog(np.arange(1,len(all_mi[6])+1), all_mi[6], label="SP16, L=100, V=4, FS={abbaabbaabbaabba,bccbbccbbccbbccb}")
 
-    # plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], label="SP2, L=20, V=4, FS={ab,bc,cd,dc}")
+    # plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], label="SP2, L=20, V=4, FS={ab,bc}")
     # plt.loglog(np.arange(1,len(all_mi[8])+1), all_mi[8], label="SP2, L=20, V=26, FS={ab,bc,cd,de,ef,fg}")
 
-    # plt.loglog(np.arange(1,len(all_mi[9])+1), all_mi[9], label="SP8, L=20, V=4")
-    # plt.loglog(np.arange(1,len(all_mi[10])+1), all_mi[10], label="SP8, L=20, V=26")
-    plt.loglog(np.arange(1,len(all_mi[9])+1), all_mi[9], label="SP8, L=20, V=4, FS={abbaabba,bccbbccb}")
-    plt.loglog(np.arange(1,len(all_mi[10])+1), all_mi[10], label="SP8, L=20, V=26, FS={abbaabba,bccbbccb,cddccddc,deeddeed,\n effeeffe,fggffggf,ffhhiikk}")
+    # plt.loglog(np.arange(1,len(all_mi[9])+1), all_mi[9], label="SP8, L=20, V=4") # FS={abbaabba,bccbbccb}
+    # plt.loglog(np.arange(1,len(all_mi[10])+1), all_mi[10], label="SP8, L=20, V=26") # FS={abbaabba,bccbbccb,cddccddc,deeddeed,\n effeeffe,fggffggf,ffhhiikk}")
 
-    # plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], label="SP2, L=20, V=4, FS={ab,bc,cd,dc}")
-    # plt.loglog(np.arange(1,len(all_mi[11])+1), all_mi[11], label="SP2, L=20, V=4, FS={ab,bc,cd,dc}, Small")
+    # plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], label="SP2, L=20, V=4, FS={ab,bc}")
+    # plt.loglog(np.arange(1,len(all_mi[11])+1), all_mi[11], label="SP2, L=20, V=4, FS={ab,bc}, Small")
 
-    # plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], label="SP2, L=100, V=4, FS={ab,bc,cd,dc}")
-    # plt.loglog(np.arange(1,len(all_mi[12])+1), all_mi[12], label="SP2, L=100, V=4, FS={ab,bc,cd,dc}, Small")
+    # plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], label="SP2, L=100, V=4, FS={ab,bc}")
+    # plt.loglog(np.arange(1,len(all_mi[12])+1), all_mi[12], label="SP2, L=100, V=4, FS={ab,bc}, Small")
 
     # plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], label="SP2, L=20, V=4, FS={ab,bc}")
     # plt.loglog(np.arange(1,len(all_mi[13])+1), all_mi[13], label="SP2, L=20, V=4, FS={ab,bc,cd,dc}")
 
-    # plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], label="SP2, L=100, V=4, FS={ab,bc}")
-    # plt.loglog(np.arange(1,len(all_mi[14])+1), all_mi[14], label="SP2, L=100, V=4, FS={ab,bc,cd,dc}")
-
-    # plt.loglog(np.arange(1,len(all_mi[15])+1), all_mi[15], label="SP2, L=20, V=4, FS={ab,bc}")
-    # plt.loglog(np.arange(1,len(all_mi[16])+1), all_mi[16], label="SP2, L=20, V=4, FS={ab,bc,cd,dc}")
+    plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], label="SP2, L=100, V=4, FS={ab,bc}")
+    plt.loglog(np.arange(1,len(all_mi[14])+1), all_mi[14], label="SP2, L=100, V=4, FS={ab,bc,cd,dc}")
 
     plt.tick_params(labelsize='large', width=5)
     plt.grid(True)
@@ -82,11 +77,9 @@ with plt.style.context(('seaborn')):
     plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
     ax = plt.axes()
     ax.set_xlim(1, 1000)
-    # ax.set_ylim(-0.005, 0.06)
-    print(ax.get_xlim())
-    print(ax.get_ylim())
+    # ax.set_ylim(ax.get_ylim()[0]-1e-7, ax.get_ylim()[1])
     ax.set_xlabel('Distance between symbols (Lag), d', fontsize=15)
     ax.set_ylabel('Mutual Information, I(d)', fontsize=15)
     lgd = ax.legend(loc='lower left', shadow=True, fancybox=True, numpoints=1, prop={'size': 12})
-    plt.savefig('spk_v1', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.savefig('spk_f1', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
