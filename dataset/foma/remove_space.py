491e287e42f043844151f43cfbc3ce7cbca7ab9f
silentknight <1491628+silentknight@users.noreply.github.com>
2022-01-17 12:55:24 +0000

    Up Images

--- dataset/foma/remove_space.py
+++ dataset/foma/remove_space.py
@@ -3,24 +3,24 @@ import random
 def myfunction():
   return 0.1
 
-filename = "Data_SP2_100.dat"
+filename = "Data_SP2_20_New_F1.dat"
 
-f = open("Old/"+filename,"r")
+f = open("trash/"+filename,"r")
 lines = f.readlines()
 f.close()
 
-no_lines = int(len(lines)/20)
-random.shuffle(lines, myfunction)
+# no_lines = int(len(lines)/20)
+# random.shuffle(lines, myfunction)
 
 new = ""
-index = 0
+# index = 0
 
 for line in lines:
 	new += line.rstrip('\n').strip()
-	if(index == no_lines):
-		break
-	index += 1
+	# if(index == no_lines):
+	# 	break
+	# index += 1
 
-f = open("r_"+filename,"w")
+f = open(filename,"w")
 f.write(new)
 f.close()
