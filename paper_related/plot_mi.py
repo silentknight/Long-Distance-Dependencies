257b6178c035731fefee7d3b8816342852b3ab7a
silentknight <1491628+silentknight@users.noreply.github.com>
2022-01-31 10:48:21 +0000

    Up new

--- paper_related/plot_mi.py
+++ paper_related/plot_mi.py
@@ -4,7 +4,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# filenames = ["mnist_grassberger_logx_mi.dat","mnist_standard_logx_mi.dat"]
 # filenames = ["penn_tree_words_10000_grassberger_logx_mi.dat","penn_tree_words_10000_standard_logx_mi.dat"]
 filenames = ["wiki2_words_10000_grassberger_logx_mi.dat","wiki2_words_10000_standard_logx_mi.dat"]
 
@@ -55,17 +54,17 @@ with plt.style.context(('seaborn')):
     plt.loglog(np.arange(1,len(all_mi[0])+1), all_mi[0], label="Using Grassbergers Equation")
     plt.loglog(np.arange(1,len(all_mi[1])+1), all_mi[1], label="Using Shannon's Equation")
 
-    plt.tick_params(labelsize='large', width=5)
+    plt.tick_params(labelsize='large', width=10)
     plt.grid(True)
     plt.grid(which='major', linestyle='-.', linewidth='0.5', color='grey')
     plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
     ax = plt.axes()
     ax.set_xlim(1, len(all_mi[0]))
-    # ax.set_ylim(0.56, 3.2)
+    ax.set_ylim(0.9, 2.7)
     print(ax.get_xlim())
     print(ax.get_ylim())
-    ax.set_xlabel('Distance between words (lag), d', fontsize=15)
-    ax.set_ylabel('Mutual Information, I(d)', fontsize=15)
-    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, ncol=3, numpoints=1, prop={'size': 12})
+    ax.set_xlabel('Distance between words (lag), d', fontsize=20)
+    ax.set_ylabel('Mutual Information, I(d)', fontsize=20)
+    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, numpoints=1, prop={'size': 18})
     plt.savefig('mi_intro_wiki2', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
