e67008aa9e45f7139fb7de660fed4e12d5bc55c6
silentknight <1491628+silentknight@users.noreply.github.com>
2022-01-31 23:01:45 +0000

    Up

--- paper_related/framework.py
+++ paper_related/framework.py
@@ -12,13 +12,13 @@ with plt.style.context(('seaborn')):
 
     ##########################################################################################################################
 
-    break_point = 5
-    alpha_1 = 0.4
-    alpha_2 = 0.05
-    amplitude = 1.2
-    f = models.BrokenPowerLaw1D(amplitude=amplitude, x_break=break_point, alpha_1=alpha_1, alpha_2=alpha_2)
-    fit_sample = f(x)
-    p1 = plt.loglog(x, fit_sample, label="Broken Power-Law Decay")
+    #break_point = 5
+    #alpha_1 = 0.4
+    #alpha_2 = 0.05
+    #amplitude = 1.2
+    #f = models.BrokenPowerLaw1D(amplitude=amplitude, x_break=break_point, alpha_1=alpha_1, alpha_2=alpha_2)
+    #fit_sample = f(x)
+    #p1 = plt.loglog(x, fit_sample, label="Broken Power-Law Decay")
 
     ##########################################################################################################################
 
@@ -29,10 +29,10 @@ with plt.style.context(('seaborn')):
 
     ##########################################################################################################################
 
-    #a = 2
-    #b = 0.1
-    #y = a*pow(b,x)
-    #p3 = plt.loglog(x, y, label="Exponential Decay")
+    a = 2
+    b = 0.1
+    y = a*pow(b,x)
+    p3 = plt.loglog(x, y, label="Exponential Decay")
 
     ##########################################################################################################################
 
@@ -48,14 +48,14 @@ with plt.style.context(('seaborn')):
 
     ##########################################################################################################################
     ax = plt.axes()
-    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, prop={'size': 6})
+    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, prop={'size': 15})
     plt.tick_params(labelsize='large', width=3)
     plt.grid(True)
     plt.grid(which='major', linestyle='-.', linewidth='0.5', color='grey')
     plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
     ax.set_xlim(1.0, 10000.0)
-    # # ax.set_ylim(0.5665692652625367, 3.5)
-    ax.set_xlabel('Distance between symbol, d', fontsize=15)
-    ax.set_ylabel('Mutual Information, I(d)', fontsize=15)
-    plt.savefig('standard_broken_pl', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    ax.set_ylim(0.1, 3)
+    ax.set_xlabel('Distance between symbol, d', fontsize=20)
+    ax.set_ylabel('Mutual Information, I(d)', fontsize=20)
+    plt.savefig('standard_exponential', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
