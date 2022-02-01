ed2eee20a25b2e99855a9d821ae47c560053830d
silentknight <1491628+silentknight@users.noreply.github.com>
2022-02-01 19:45:33 +0000

    Up

--- paper_related/framework.py
+++ paper_related/framework.py
@@ -22,10 +22,10 @@ with plt.style.context(('seaborn')):
 
     ##########################################################################################################################
 
-    #a = 2
-    #alpha = 0.1
-    #y = a*pow(x,-alpha)
-    #p2 = plt.loglog(x, y, label="Power-Law Decay")
+    a = 2.5
+    alpha = 1.2
+    y = a*pow(x,-alpha)
+    p2 = plt.loglog(x, y, label="Power-Law Decay")
 
     ##########################################################################################################################
 
@@ -36,16 +36,16 @@ with plt.style.context(('seaborn')):
 
     ##########################################################################################################################
 
-    x = np.linspace(1, 1000, 1000)
-    y = np.zeros(1000)
-    a = 2
-    alpha_1 = 0.9
-    alpha_2 = -1
-    for i in range(0,1000,50):
-        y[i:i+35] = a*pow(x[0:35],-alpha_1)
-        y[i+35:i+50] = y[i+34]*pow(x[0:15],-alpha_2)
-        a = y[i+49]
-    p4 = plt.loglog(x, y, label="Power-Law Decay with Periodicity")
+    #x = np.linspace(1, 1000, 1000)
+    #y = np.zeros(1000)
+    #a = 2
+    #alpha_1 = 0.9
+    #alpha_2 = -1
+    #for i in range(0,1000,50):
+    #    y[i:i+35] = a*pow(x[0:35],-alpha_1)
+    #    y[i+35:i+50] = y[i+34]*pow(x[0:15],-alpha_2)
+    #    a = y[i+49]
+    #p4 = plt.loglog(x, y, label="Power-Law Decay with Periodicity")
 
     ##########################################################################################################################
     ax = plt.axes()
@@ -55,8 +55,8 @@ with plt.style.context(('seaborn')):
     plt.grid(which='major', linestyle='-.', linewidth='0.5', color='grey')
     plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
     ax.set_xlim(1.0, 1000.0)
-    # ax.set_ylim(0.1, 3)
+    ax.set_ylim(0.1, 3)
     ax.set_xlabel('Distance between symbol, d', fontsize=20)
     ax.set_ylabel('Mutual Information, I(d)', fontsize=20)
-    plt.savefig('standard_pl_periodic', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.savefig('pl_deep', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
