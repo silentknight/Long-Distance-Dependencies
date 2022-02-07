e24da77e088644dbc112f560bc129c30053795c7
silentknight <1491628+silentknight@users.noreply.github.com>
2022-02-08 00:35:37 +0530

    Up

--- data.py
+++ data.py
@@ -181,8 +181,25 @@ class Corpus(object):
 			self.process_mobility(dataset)
 
 		elif path == "dataset/mnist_data/":
+			print("Sequential MNIST dataset")
 			dataset = os.path.join(path, 'mnist_data_un.dat')
 			self.tokenize_file(dataset)
+
+		elif path == "dataset/cifar10/red":
+			print("CIFAR 10 Dataset, Red channel")
+			self.process_cifar_10(red)
+
+		elif path == "dataset/cifar10/green":
+			print("CIFAR 10 Dataset, Green channel")
+			self.process_cifar_10(green)
+
+		elif path == "dataset/cifar10/blue":
+			print("CIFAR 10 Dataset, Blue channel")
+			self.process_cifar_10(blue)
+
+		elif path == "dataset/cifar10/gray":
+			print("CIFAR 10 Dataset, Grayscale channel")
+			self.process_cifar_10(gray)
 
 		elif path == "dataset/copy_add/":
 			dataset = os.path.join(path, 'copy.dat')
@@ -307,3 +324,58 @@ class Corpus(object):
 			self.sequentialData.add_to_list(wordID)
 		self.sequentialData.add_data()
 		print("Size of Vocabulary", len(self.dictionary.counter))
+
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        data = pickle.load(fo, encoding='bytes')
+    return data
+
+
+def load_cifar_10_data():
+    data_dir = "."
+    meta_data_dict = unpickle(data_dir + "/batches.meta")
+    cifar_label_names = meta_data_dict[b'label_names']
+    cifar_label_names = np.array(cifar_label_names)
+
+    cifar_train_data = None
+    cifar_train_filenames = []
+    cifar_train_labels = []
+
+    for i in range(1, 6):
+        cifar_train_data_dict = unpickle(data_dir + "/data_batch_{}".format(i))
+        if i == 1:
+            cifar_train_data = cifar_train_data_dict[b'data']
+        else:
+            cifar_train_data = np.vstack((cifar_train_data, cifar_train_data_dict[b'data']))
+        cifar_train_filenames += cifar_train_data_dict[b'filenames']
+        cifar_train_labels += cifar_train_data_dict[b'labels']
+
+    cifar_test_data_dict = unpickle(data_dir + "/test_batch")
+    cifar_test_data = cifar_test_data_dict[b'data']
+    cifar_test_filenames = cifar_test_data_dict[b'filenames']
+    cifar_test_labels = cifar_test_data_dict[b'labels']
+
+    return cifar_train_data, cifar_test_data
+
+
+def process_cifar_10(channel):
+    train_data, test_data = load_cifar_10_data()
+    data = np.concatenate((train_data, test_data), axis=0)
+
+    for i in range(data.shape[0]):
+        [data_red, data_green, data_blue] = np.split(data[i],3)
+        data_grayscale = 0.299*data_red + 0.587*data_green + 0.114*data_blue
+
+        if channel == "red":
+            pixel_values = data_red
+        elif channel == "green":
+            pixel_values = data_green
+        elif channel == "red":
+            pixel_values = data_blue
+        elif channel == "gray":
+            pixel_values = data_grayscale
+
+        string_pixels = [str(pix) for pix in pixel_values]
+        pixel_line = " ".join(string_pixels)
+        self.tokenize_strings(pixel_line)
