7f9846b7afb42961ceae79850171efc92da0bb9d
silentknight <1491628+silentknight@users.noreply.github.com>
2022-02-14 01:56:38 +0530

    Up

--- paper_related/plot_words.py
+++ paper_related/plot_words.py
@@ -4,7 +4,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-filenames = ["penn_tree","text8","text8_subset","text8_wor","text8_subset_wor","wiki2","wiki2_cleaned","wiki2_raw","wiki2_PTB","wiki103","wiki103_cleaned","wiki103_raw","wiki19","wiki19_cleaned","wiki19_Text8","10kGNAD","one_billion"]
+filenames = ["penn_tree","text8","text8_wor","text8_subset","text8_subset_wor","wiki2","wiki2_raw","wiki2_cleaned", \
+            "wiki19","wiki19_Text8","wiki103","wiki103_raw","wiki103_cleaned","wiki_sample_1","wiki_sample_2", \
+            "wiki_PTB_size_1","wiki_PTB_size_2","wiki_PTB_vocab_1","wiki_PTB_vocab_2","10kGNAD"]
 
 all_mi = []
 all_hx = []
@@ -50,23 +52,32 @@ for filename in filenames:
 
 with plt.style.context(('seaborn')):
 
-    plt.loglog(np.arange(1,len(all_mi[0])+1), all_mi[0], "r-", label="PTB")
-    plt.loglog(np.arange(1,len(all_mi[1])+1), all_mi[1], 'm-', label="Text8")
-    plt.loglog(np.arange(1,len(all_mi[2])+1), all_mi[2], 'm--', label="Text8 (S)")
-    plt.loglog(np.arange(1,len(all_mi[3])+1), all_mi[3], 'c-', label="Text8 (wR)")
-    plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], 'c--', label="Text8 (wR/S)")
-    plt.loglog(np.arange(1,len(all_mi[5])+1), all_mi[5], "b-", label="Wiki2")
-    plt.loglog(np.arange(1,len(all_mi[6])+1), all_mi[6], "b--", label="Wiki2 (C)")
-    plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], "b:", label="Wiki2 (Raw)")
-    plt.loglog(np.arange(1,len(all_mi[8])+1), all_mi[8], "b-.", label="Wiki2 (PTB)")
-    plt.loglog(np.arange(1,len(all_mi[9])+1), all_mi[9], "g-", label="Wiki103")
-    plt.loglog(np.arange(1,len(all_mi[10])+1), all_mi[10], "g--", label="Wiki103 (C)")
-    plt.loglog(np.arange(1,len(all_mi[11])+1), all_mi[11], "g:", label="Wiki103 (Raw)")
-    plt.loglog(np.arange(1,len(all_mi[12])+1), all_mi[12], "k-", label="Wiki19")
-    plt.loglog(np.arange(1,len(all_mi[13])+1), all_mi[13], "k--", label="Wiki19 (C)")
-    plt.loglog(np.arange(1,len(all_mi[14])+1), all_mi[14], "k-.", label="Wiki19 (Text8)")
-    plt.loglog(np.arange(1,len(all_mi[15])+1), all_mi[15], "y--", label="German Text")
-    plt.loglog(np.arange(1,len(all_mi[16])+1), all_mi[16], "y-", label="One Billion Words")
+    plt.loglog(np.arange(1,len(all_mi[0])+1), all_mi[0], color='red', linestyle='solid', label="PTB")
+
+    plt.loglog(np.arange(1,len(all_mi[1])+1), all_mi[1], color='green', linestyle='dashed', label="Text8")
+    plt.loglog(np.arange(1,len(all_mi[2])+1), all_mi[2], color='green', linestyle='solid', label="Text8 (w/ Rare)")
+    plt.loglog(np.arange(1,len(all_mi[3])+1), all_mi[3], color='darkgreen', linestyle='dashed', label="Text8 (Subset)")
+    plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], color='darkgreen', linestyle='solid', label="Text8 (w/ Rare Ssubset)")
+
+    plt.loglog(np.arange(1,len(all_mi[5])+1), all_mi[5], color='cyan', linestyle='solid', label="Wiki2")
+    plt.loglog(np.arange(1,len(all_mi[6])+1), all_mi[6], color='cyan', linestyle='dashed', label="Wiki2 (Raw)")
+    plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], color='cyan', linestyle='dashdot', label="Wiki2 (Cleaned)")
+
+    plt.loglog(np.arange(1,len(all_mi[8])+1), all_mi[8], color='blue', linestyle='solid', label="Wiki19")
+    plt.loglog(np.arange(1,len(all_mi[9])+1), all_mi[9], color='blue', linestyle='dotted', label="Wiki19 (Text8)")
+    
+    plt.loglog(np.arange(1,len(all_mi[10])+1), all_mi[10], color='navy', linestyle='solid', label="Wiki103")
+    plt.loglog(np.arange(1,len(all_mi[11])+1), all_mi[11], color='navy', linestyle='dashed', label="Wiki103 (Raw)")
+    plt.loglog(np.arange(1,len(all_mi[12])+1), all_mi[12], color='navy', linestyle='dashdot', label="Wiki103 (Cleaned)")
+
+    plt.loglog(np.arange(1,len(all_mi[13])+1), all_mi[13], color='black', linestyle='solid', label="Wiki Sample 1")
+    plt.loglog(np.arange(1,len(all_mi[14])+1), all_mi[14], color='grey', linestyle='solid', label="Wiki Sample 2")
+    plt.loglog(np.arange(1,len(all_mi[15])+1), all_mi[15], color='black', linestyle='dashed', label="Wiki PTB Size 1")
+    plt.loglog(np.arange(1,len(all_mi[16])+1), all_mi[16], color='grey', linestyle='dashed', label="Wiki PTB Size 2")
+    plt.loglog(np.arange(1,len(all_mi[17])+1), all_mi[17], color='black', linestyle='dashdot', label="Wiki PTB Vocabulary 1")
+    plt.loglog(np.arange(1,len(all_mi[18])+1), all_mi[18], color='grey', linestyle='dashdot', label="Wiki PTB Vocabulary 2")
+                
+    plt.loglog(np.arange(1,len(all_mi[19])+1), all_mi[19], color='purple', linestyle='solid', label="German Text")
 
     plt.tick_params(labelsize='large', width=5)
     plt.grid(True)
@@ -74,7 +85,7 @@ with plt.style.context(('seaborn')):
     plt.grid(which='minor', linestyle=':', linewidth='0.2', color='grey')
     ax = plt.axes()
     ax.set_xlim(1, len(all_mi[0]))
-    # ax.set_ylim(0.56, 3.2)
+    ax.set_ylim(0.56, 3.2)
     print(ax.get_xlim())
     print(ax.get_ylim())
     ax.set_xlabel('Distance between words, d', fontsize=15)
