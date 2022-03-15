0f65f80a11099c92ee2925dc574e621d0b2a83ef
silentknight <1491628+silentknight@users.noreply.github.com>
2022-03-15 18:05:03 +0000

    Up

--- paper_related/framework.py
+++ paper_related/framework.py
@@ -12,13 +12,13 @@ with plt.style.context(('seaborn')):
 
     ##########################################################################################################################
 
-    break_point = 100
-    alpha_1 = 0.04
-    alpha_2 = 0.2
-    amplitude = 7
-    f = models.BrokenPowerLaw1D(amplitude=amplitude, x_break=break_point, alpha_1=alpha_1, alpha_2=alpha_2)
-    fit_sample = f(x)
-    p1 = plt.loglog(x, fit_sample, label="Broken Power-Law Decay")
+    # break_point = 100
+    # alpha_1 = 0.04
+    # alpha_2 = 0.2
+    # amplitude = 7
+    # f = models.BrokenPowerLaw1D(amplitude=amplitude, x_break=break_point, alpha_1=alpha_1, alpha_2=alpha_2)
+    # fit_sample = f(x)
+    # p1 = plt.loglog(x, fit_sample, label="Broken Power-Law Decay")
 
     ##########################################################################################################################
 
@@ -49,6 +49,20 @@ with plt.style.context(('seaborn')):
     # p4 = plt.loglog(x, y, label="Power-Law Decay with Periodicity")
 
     ##########################################################################################################################
+
+    x = np.linspace(1, 1000, 1000)
+    a = 1
+    alpha_1 = 0.9
+    alpha_2 = -1
+    y = power
+    for i in range(0,1000,50):
+    #    y[i:i+35] = a*pow(x[0:35],-alpha_1)
+    #    y[i+35:i+50] = y[i+34]*pow(x[0:15],-alpha_2)
+    #    a = y[i+49]
+    p4 = plt.loglog(x, y, label="Power-Law Decay with Periodicity")
+
+    ##########################################################################################################################
+
     ax = plt.axes()
     lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, prop={'size': 15})
     plt.tick_params(labelsize='large', width=3)
