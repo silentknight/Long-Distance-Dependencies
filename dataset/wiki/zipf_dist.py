1b5b5a6a81b8b4c51f769bc359d51e740deb504b
silentknight <1491628+silentknight@users.noreply.github.com>
2022-10-03 17:09:27 +0530

    Up

--- dataset/wiki/zipf_dist.py
+++ dataset/wiki/zipf_dist.py
@@ -1,12 +1,17 @@
 import re
 from operator import itemgetter
 
-dir = "wikitext-19L/"
+dir = "wikitext-103/"
 
 frequency = {}
 open_file = open(dir+'train', 'r')
 file_to_string = open_file.read()
-words = re.findall(r'(\b[A-Za-z][a-z]{2,9}\b)', file_to_string)
+open_file = open(dir+'test', 'r')
+file_to_string += open_file.read()
+open_file = open(dir+'valid', 'r')
+file_to_string += open_file.read()
+#words = re.findall(r'(\b[A-Za-z][a-z]{2,9}\b)', file_to_string)
+words = file_to_string.split()
 
 for word in words:
     count = frequency.get(word,0)
