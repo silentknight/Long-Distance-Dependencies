1a530e10af335e408076d004f870bdb7603bf0b1
silentknight <1491628+silentknight@users.noreply.github.com>
2022-10-03 17:08:49 +0530

    Up

--- paper_related/plot_words.py
+++ paper_related/plot_words.py
@@ -53,15 +53,23 @@ for filename in filenames:
 with plt.style.context(('seaborn')):
     ax = plt.axes()
 
+    # plt.loglog(np.arange(1,len(all_mi[0])+1), all_mi[0], label="PTB")
+
+    plt.loglog(np.arange(1,len(all_mi[1])+1), all_mi[1], label="Text8")
+    plt.loglog(np.arange(1,len(all_mi[3])+1), all_mi[3], label="Text8 (Small)")
+    plt.loglog(np.arange(1,len(all_mi[2])+1), all_mi[2], label="Text8 (w/o Rare)")
+    plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], label="Text8 (Small w/o Rare)")
+
+    # plt.loglog(np.arange(1,len(all_mi[8])+1), all_mi[8], label="Wiki19")
+    # plt.loglog(np.arange(1,len(all_mi[9])+1), all_mi[9], label="Wiki19 (Text8)")
+
     # plt.loglog(np.arange(1,len(all_mi[6])+1), all_mi[6], label="Wiki2 (Raw)")
     # plt.loglog(np.arange(1,len(all_mi[5])+1), all_mi[5], label="Wiki2")
     # plt.loglog(np.arange(1,len(all_mi[7])+1), all_mi[7], label="Wiki2 (Cleaned)")
 
-    # plt.loglog(np.arange(1,len(all_mi[8])+1), all_mi[8], label="Wiki19")
-
-    plt.loglog(np.arange(1,len(all_mi[11])+1), all_mi[11], label="Wiki103 (Raw)")
-    plt.loglog(np.arange(1,len(all_mi[10])+1), all_mi[10], label="Wiki103")
-    plt.loglog(np.arange(1,len(all_mi[12])+1), all_mi[12], label="Wiki103 (Cleaned)")
+    # plt.loglog(np.arange(1,len(all_mi[11])+1), all_mi[11], label="Wiki103 (Raw)")
+    # plt.loglog(np.arange(1,len(all_mi[10])+1), all_mi[10], label="Wiki103")
+    # plt.loglog(np.arange(1,len(all_mi[12])+1), all_mi[12], label="Wiki103 (Cleaned)")
 
     # plt.loglog(np.arange(1,len(all_mi[13])+1), all_mi[13], label="Wiki Sample 1")
     # plt.loglog(np.arange(1,len(all_mi[14])+1), all_mi[14], label="Wiki Sample 2")
@@ -69,14 +77,6 @@ with plt.style.context(('seaborn')):
     # plt.loglog(np.arange(1,len(all_mi[16])+1), all_mi[16], label="Wiki PTB like Size 2")
     # plt.loglog(np.arange(1,len(all_mi[17])+1), all_mi[17], label="Wiki PTB like Vocabulary 1")
     # plt.loglog(np.arange(1,len(all_mi[18])+1), all_mi[18], label="Wiki PTB like Vocabulary 2")
-
-    # plt.loglog(np.arange(1,len(all_mi[0])+1), all_mi[0], label="PTB")
-
-    # plt.loglog(np.arange(1,len(all_mi[1])+1), all_mi[1], label="Text8")
-    # plt.loglog(np.arange(1,len(all_mi[2])+1), all_mi[2], label="Text8 (w/ Rare)")
-    # plt.loglog(np.arange(1,len(all_mi[3])+1), all_mi[3], label="Text8 (Small)")
-    # plt.loglog(np.arange(1,len(all_mi[4])+1), all_mi[4], label="Text8 (w/ Rare Small)")
-    # plt.loglog(np.arange(1,len(all_mi[9])+1), all_mi[9], label="Wiki19 (Text8)")
 
     # plt.loglog(np.arange(1,len(all_mi[19])+1), all_mi[19], label="German Text")
 
@@ -90,6 +90,6 @@ with plt.style.context(('seaborn')):
     print(ax.get_ylim())
     ax.set_xlabel('Distance between words, d', fontsize=15)
     ax.set_ylabel('Mutual Information, I(d)', fontsize=15)
-    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, ncol=3, numpoints=1, prop={'size': 12})
-    # plt.savefig('lm_words_wiki2_ptb', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, ncol=2, numpoints=1, prop={'size': 12})
+    plt.savefig('lm_words_text8', bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.show()
