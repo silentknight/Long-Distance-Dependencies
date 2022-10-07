59a04358f0cf4772513e2576b0e2aeb2df2e81c9
silentknight <1491628+silentknight@users.noreply.github.com>
2022-10-07 21:48:22 +0530

    Up

--- data.py
+++ data.py
@@ -155,7 +155,7 @@ class Corpus(object):
 			self.valid = self.tokenize_file(os.path.join(path, 'valid-pos'))
 			self.test = self.tokenize_file(os.path.join(path, 'test-pos'))
 		elif path == "dataset/paper_dataset/":
-			dataset = self.tokenize_file(os.path.join(path, 'wikitext-m/cleaned_train_2'))
+			dataset = self.tokenize_file(os.path.join(path, 'wiki_sample_3'))
 		elif path == "dataset/m-wikitext-2/":
 			dataset = self.tokenize_file(os.path.join(path, 'train'))
 
