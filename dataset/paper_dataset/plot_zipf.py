f7a6a3a03b1459073c3ed7290ae1a9e5519ef4aa
silentknight <1491628+silentknight@users.noreply.github.com>
2022-12-20 15:22:14 +0000

    Up

--- dataset/paper_dataset/plot_zipf.py
+++ dataset/paper_dataset/plot_zipf.py
@@ -7,10 +7,12 @@ from scipy import optimize
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
-filenames = ['wiki2', 'wiki_sample_1', 'wiki_sample_2', 'wiki_sample_3', 'wiki_sample_4']
+# filenames = ['wiki2', 'wiki_sample_1', 'wiki_sample_2', 'wiki_sample_3', 'wiki_sample_4']
 # filenames = ['SP2_V4_20','SP4_V4_20','SP2_V26_20','SP4_V26_20']
 # filenames = ['wiki103','wiki2']
 # filenames = ['text8','text8-small','text8-small-wo-r-2','text8-small-wo-r-4']
+filenames = ['text8', 'wikitext-19L', 'text8-wo-r-2', 'wikitext-19L-wo-r-2', 'wikitext-19']
+
 all_words = []
 all_counts = []
 max_len = 0
@@ -64,19 +66,19 @@ for filename in filenames:
 with plt.style.context(('seaborn')):
 	ax = plt.axes()
 
-	plt.loglog(np.arange(1,len(all_counts[0])+1), all_counts[0], label="WikiText2: "+r'$\alpha$'+" = -1.023")
+	plt.loglog(np.arange(1,len(all_counts[0])+1), all_counts[0], label="Text8: "+r'$\alpha$'+" = -1.056")
 	# plt.loglog(all_counts[0], powerlaw(np.array(all_counts[0]).astype(float), 1e6, -1.056310487080293), label="First Fit")
 
-	plt.loglog(np.arange(1,len(all_counts[1])+1), all_counts[1], label="WikiText Sample 1: "+r'$\alpha$'+" = -1.035")
+	plt.loglog(np.arange(1,len(all_counts[1])+1), all_counts[1], label="Wiki19 (Text8-like): "+r'$\alpha$'+" = -1.077")
 	# plt.loglog(all_counts[1], powerlaw(np.array(all_counts[1]).astype(float), 0.2*1e6, -1.05507833693917), label="Second Fit")
 
-	plt.loglog(np.arange(1,len(all_counts[2])+1), all_counts[2], label="WikiText Sample 2: "+r'$\alpha$'+" = -1.028")
+	plt.loglog(np.arange(1,len(all_counts[2])+1), all_counts[2], label="Text8 (w/o Rare): "+r'$\alpha$'+" = -1.058")
 	# plt.loglog(all_counts[2], powerlaw(np.array(all_counts[2]).astype(float), 0.2*1e6, -1.0573763273014645), label="Third Fit")
 
-	plt.loglog(np.arange(1,len(all_counts[3])+1), all_counts[3], label="WikiText Sample 3: "+r'$\alpha$'+" = -1.020")
+	plt.loglog(np.arange(1,len(all_counts[3])+1), all_counts[3], label="Wiki19 (Text8-like w/o Rare): "+r'$\alpha$'+" = -1.078")
 	# plt.loglog(all_counts[3], powerlaw(np.array(all_counts[3]).astype(float), 0.2*1e6, -1.0576934993354605), label="Fourth Fit")
 
-	plt.loglog(np.arange(1,len(all_counts[4])+1), all_counts[4], label="WikiText Sample 4: "+r'$\alpha$'+" = -1.027")
+	plt.loglog(np.arange(1,len(all_counts[4])+1), all_counts[4], label="Wiki19: "+r'$\alpha$'+" = -1.034")
 	# plt.loglog(all_counts[3], powerlaw(np.array(all_counts[3]).astype(float), 0.2*1e6, -1.0576934993354605), label="Fourth Fit")
 
 	plt.tick_params(labelsize='large', width=5)
@@ -87,5 +89,5 @@ with plt.style.context(('seaborn')):
 	plt.xlabel('Word Rank, r', fontsize=15)
 	plt.ylabel('Frequency, F(r)', fontsize=15)
 	lgd = ax.legend(loc='upper right', shadow=True, fancybox=True, ncol=1, numpoints=1, prop={'size': 12})
-	plt.savefig('zipf_plots_wiki_samples', bbox_extra_artists=(lgd,), bbox_inches='tight')
+	plt.savefig('zipf_plots_wiki_text8', bbox_extra_artists=(lgd,), bbox_inches='tight')
 	plt.show()
